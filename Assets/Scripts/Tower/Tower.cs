6bd739e8e0187d463cfa11bc3f98c3664832ff82
Fedor Ptitsyn <fedya.ptitsyn@mail.ru>
2024-09-23 00:45:48 +0300

    Started working on towers :tada:

---
+++ Assets/Scripts/Tower/Tower.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Tower
+{
+    public class Tower : MonoBehaviour
+    {
+        private float _damage;
+        private float _attackRange;
+        private float _fireRate;
+        private AttackType _attackType;
+        private GameObject _currentTarget;
+
+        private void Update()
+        {
+
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            // Maybe check for collider type
+            float distance = Vector3.Distance(transform.position, other.transform.position);
+            
+        }
+    }
+
+    enum AttackType
+    {
+        
+    }
+}
