e3cc34615b3708622aaee9f3e467bdd0c76d37dc
Niston <vadimpavlov@bk.ru>
2024-09-23 19:47:40 +0300

    Anim for running and enemy model

--- Assets/Scripts/Enemies/StateMachine/RunningState.cs
+++ Assets/Scripts/Enemies/StateMachine/RunningState.cs
@@ -9,11 +9,13 @@ public class RunningState : EnemyState
     {
         _target = GameObject.FindGameObjectWithTag("Castle").transform;
         navMeshAgent.speed = 1;
+        animator.Play("mixamo_com");
     }
     
     public override void Do()
     {
         navMeshAgent.destination = _target.position;
+        animator.speed = Helpers.Map(navMeshAgent.speed - 0.43f, 0, 1, 0, 1.6f, true);
     }
     
     public override void Exit()
