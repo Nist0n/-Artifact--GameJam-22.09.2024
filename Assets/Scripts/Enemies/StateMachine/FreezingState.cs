1550aa48bd046a77b189444488116ef371d5691d
Niston <vadimpavlov@bk.ru>
2024-09-23 23:13:22 +0300

    Reapply "Freezing State"
    This reverts commit b169cb0dbcccabb24ec769f098ab72bc8cc08bcf.

--- Assets/Scripts/Enemies/StateMachine/FreezingState.cs
+++ Assets/Scripts/Enemies/StateMachine/FreezingState.cs
@@ -4,12 +4,18 @@ public class FreezingState : EnemyState
 {
     public override void Enter()
     {
-        
+        navMeshAgent.speed = 0f;
+        animator.speed = 0f;
     }
     
     public override void Do()
     {
-        
+        if (!IsFreezed)
+        {
+            navMeshAgent.speed = 1f;
+            animator.speed = 1f;
+            IsComplete = true;
+        }
     }
     
     public override void Exit()
