a9dab595811cd07c07ea5fb4e6bdfebcb85cc3e0
IDidThisShit <krupinvlad.2005@gmail.com>
2024-09-24 20:16:41 +0300

    SomeSoundAndUIRevork

---
+++ Assets/Scripts/Sound/ClickSound.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ClickSound : MonoBehaviour
+{
+    public void PlaySoundHighlited()
+    {
+        AudioManager.instance.PlaySFX("Aimed");
+    }
+    public void PlaySoundPressed()
+    {
+        AudioManager.instance.PlaySFX("Click");
+    }
+}
--- Assets/Scripts/Sound/VolumeSettings.cs
+++ Assets/Scripts/Sound/VolumeSettings.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -8,11 +9,39 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] Slider musicSlider;
     [SerializeField] Slider SFXSlider;
 
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume") && PlayerPrefs.HasKey("SFXVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+    }
+
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
-        audioMixer.SetFloat("music", Mathf.Log10(volume)*20);
+        audioMixer.SetFloat("music", MathF.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
     }
 
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        audioMixer.SetFloat("SFX", MathF.Log10(volume)*20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
 
+    private void LoadVolume()
+    {
+        float musicVolume = PlayerPrefs.GetFloat("musicVolume");
+        musicSlider.value = musicVolume;
+
+        float sfxVolume = PlayerPrefs.GetFloat("SFXVolume");
+        SFXSlider.value = sfxVolume;
+    }
 }
