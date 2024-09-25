ac8b2f916191f2c5f4ef05b7743240f328fe012a
Niston <vadimpavlov@bk.ru>
2024-09-25 21:09:15 +0300

    Spawners spawning

--- Assets/Scripts/GameConfig/GameConfig.cs
+++ Assets/Scripts/GameConfig/GameConfig.cs
@@ -1,24 +1,66 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public abstract class GameConfig : MonoBehaviour
+public class GameConfig : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> enemiesTypes;
+
     public float GameTime;
 
-    public List<GameObject> enemyList;
+    public List<GameObject> EnemyList;
+
+    [SerializeField] private List<Spawner> _spawners;
+
+    private int _countOfUnits;
+
+    private bool IsFirstWaveStarted = false;
+
+    private void Start()
+    {
+        GetSpawners();
+    }
 
     private void Update()
     {
         GameTime += Time.deltaTime;
+        GameBrain();
     }
-    
-    public void SetupInstances()
+
+    private void GetSpawners()
     {
-        Spawner[] allchildSpawners = GetComponentsInChildren<Spawner>();
-        foreach (Spawner spawner in allchildSpawners)
+        var spawn = GetComponentsInChildren<Spawner>();
+        Debug.Log(spawn.Length);
+        foreach (var spawner in spawn)
         {
-            spawner.SetGameConfig(this);
+            Debug.Log(spawner);
+            _spawners.Add(spawner);
+        }
+    }
+
+    private void GameBrain()
+    {
+        if (GameTime > 3f && !IsFirstWaveStarted)
+        {
+            _countOfUnits = Random.Range(10, 15);
+            GetSummoners(_countOfUnits);
+            foreach (var spawner in _spawners)
+            {
+                StartCoroutine(spawner.StartSpawn());
+            }
+
+            IsFirstWaveStarted = true;
+        }
+    }
+
+    private void GetSummoners(int enemyCount)
+    {
+        for (int i = 0; i < enemyCount; i++)
+        {
+            var randSpawner = Random.Range(0, _spawners.Count);
+            var randEnemy = Random.Range(0, enemiesTypes.Count);
+            _spawners[randSpawner].SetEnemies(enemiesTypes[randEnemy]);
         }
     }
 }
--- Assets/Scripts/GameConfig/Spawners/Spawner.cs
+++ Assets/Scripts/GameConfig/Spawners/Spawner.cs
@@ -1,17 +1,45 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
+    [SerializeField] private GameObject bubble;
+
+    private Vector3 _transformBubble;
+    
     private GameConfig _gameConfig;
+
+    [SerializeField] private List<GameObject> _enemies;
 
     private void Start()
     {
-        
+        _transformBubble = transform.position;
+        _transformBubble.y += 1f;
     }
 
-    public void SetGameConfig(GameConfig gameConfig)
+    public IEnumerator StartSpawn()
     {
-        _gameConfig = gameConfig;
+        foreach (var enemy in _enemies)
+        {
+            StartCoroutine(SpawnEnemy(enemy));
+            Instantiate(bubble, _transformBubble, Quaternion.identity, transform);
+            var randTime = Random.Range(2, 7f);
+            yield return new WaitForSeconds(randTime);
+        }
+        _enemies.Clear();
+    }
+
+    public void SetEnemies(GameObject enemy)
+    {
+        _enemies.Add(enemy);
+    }
+
+    private IEnumerator SpawnEnemy(GameObject enemy)
+    {
+        yield return new WaitForSeconds(3.75f);
+        Instantiate(enemy, transform.position, Quaternion.identity, transform);
     }
 }
