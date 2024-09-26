6a104b7c6ed7ba50769915928243a0553bc2f3a1
IDidThisShit <krupinvlad.2005@gmail.com>
2024-09-26 18:16:40 +0300

    SomeChangesUIScripting

--- Assets/Scripts/UI/InGame/GamePause.cs
+++ Assets/Scripts/UI/InGame/GamePause.cs
@@ -1,4 +1,5 @@
 using Towers;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,14 +7,11 @@ public class GamePause : MonoBehaviour
 {
     [SerializeField] GameObject pauseButton;
     [SerializeField] GameObject background;
-    [SerializeField] GameObject canvasUI;
+    [SerializeField] GameObject continueGameButton;
+    [SerializeField] GameObject shopUI;
+    [SerializeField] CinemachineCamera camera;
 
     public static bool gameIsPaused;
-    private Tower _isPiloted;
-
-    private void Start()
-    {
-    }
 
     private void Update()
     {
@@ -25,12 +23,26 @@ public class GamePause : MonoBehaviour
             background.SetActive(!background.activeSelf);
         }
 
-        if (Input.GetKeyDown(KeyCode.B) && _isPiloted.piloted == false)
+        if (Input.GetKeyDown(KeyCode.B) && camera.IsLive && !gameIsPaused)
         {
-            gameIsPaused = !gameIsPaused;
-            PauseGame();
-            pauseButton.SetActive(!pauseButton.activeSelf);
-            background.SetActive(!background.activeSelf);
+            Invoke("ShopSwitch", 1f);
+        }
+
+        if (pauseButton.activeSelf)
+        {
+            if (pauseButton.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.name == "Selected")
+            {
+                Invoke("OpenSettings", 0.2f);
+            }
+
+        }
+        if (background.activeSelf)
+        {
+            if (continueGameButton.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.name == "Selected")
+            {
+                Debug.Log("Workaet");
+                Invoke("CloseSettings", 0.2f);
+            }
         }
     }
 
@@ -45,9 +57,36 @@ public class GamePause : MonoBehaviour
             Time.timeScale = 1;
         }
     }
-
     public void ResumeGame()
     {
         Time.timeScale = 1;
+    }
+
+    public void ShopSwitch()
+    {
+        pauseButton.SetActive(!pauseButton.activeSelf);
+        shopUI.SetActive(!shopUI.activeSelf);
+    }
+
+    public void OpenSettings()
+    {
+        gameIsPaused = !gameIsPaused;
+        PauseGame();
+        pauseButton.SetActive(false);
+        background.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        Debug.Log("Workaet2");
+        ResumeGame();
+        pauseButton.SetActive(true);
+        background.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("UI VLAD");
+        ResumeGame();
     }
 }
