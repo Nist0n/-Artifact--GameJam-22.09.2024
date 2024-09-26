22f826a7096db7c8cbd0b0c42c5db8dfcf77fbf3
Fedor Ptitsyn <fedya.ptitsyn@mail.ru>
2024-09-26 22:19:57 +0300

    Various improvements :+1:
    - Added a placeholder transition when starting new game;
    - Fixed bug with camera during pause.

--- Assets/Scripts/UI/MainMenu/ButtonManager.cs
+++ Assets/Scripts/UI/MainMenu/ButtonManager.cs
@@ -5,11 +5,15 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Cinemachine;
 
 public class ButtonManager : MonoBehaviour
 {
-    [SerializeField] GameObject mainMenuButtonsGroup;
-    [SerializeField] GameObject settingsButtonsGroup;
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private GameObject mainMenuButtonsGroup;
+    [SerializeField] private GameObject settingsButtonsGroup;
+    
+    [SerializeField] private CinemachineCamera sphereCamera;
 
     private void Update()
     {
@@ -17,15 +21,22 @@ public class ButtonManager : MonoBehaviour
         {
             settingsButtonsGroup.SetActive(!settingsButtonsGroup.activeSelf);
             mainMenuButtonsGroup.SetActive(!mainMenuButtonsGroup.activeSelf);
-
         }
     }
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("MainMap");
+        StartCoroutine(ChangeCamera());
     }
 
+    private IEnumerator ChangeCamera()
+    {
+        sphereCamera.Priority = 1;
+        canvas.GetComponent<Canvas>().enabled = false;
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("MainMap");
+    }
+    
     public void QuitGame()
     {
         Application.Quit();
