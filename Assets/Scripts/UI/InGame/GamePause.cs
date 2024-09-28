d88d334dd8e37b937fe0626b4d733bc192d8a22c
IDidThisShit <krupinvlad.2005@gmail.com>
2024-09-28 14:43:58 +0300

    LoliCon

--- Assets/Scripts/UI/InGame/GamePause.cs
+++ Assets/Scripts/UI/InGame/GamePause.cs
@@ -102,6 +102,7 @@ public class GamePause : MonoBehaviour
 
     public void CloseSettings()
     {
+        gameIsPaused = !gameIsPaused;
         pauseButton.SetActive(true);
         background.SetActive(false);
         continueGameButton.SetActive(false);
