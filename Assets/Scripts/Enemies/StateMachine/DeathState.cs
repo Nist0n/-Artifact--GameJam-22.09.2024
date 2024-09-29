2e9d64110286449d9e569c8ab54cef2bf33544ad
Niston <vadimpavlov@bk.ru>
2024-09-29 19:36:12 +0300

    gg

--- Assets/Scripts/Enemies/StateMachine/DeathState.cs
+++ Assets/Scripts/Enemies/StateMachine/DeathState.cs
@@ -6,6 +6,7 @@ public class DeathState : EnemyState
 {
     public override void Enter()
     {
+        AudioManager.instance.PlayLocalSound("Die", AudioSource);
         navMeshAgent.speed = 0;
         SoulsCounter.Instance.AddNightmares(Mathf.Round(Random.Range(DroppedCoinsMin, DroppedCoinsMax)));
         Instantiate(soul, transform.position, quaternion.identity);
