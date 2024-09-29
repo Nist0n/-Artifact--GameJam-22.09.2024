a54a1363562059cf1dd1670741bd6bb5a50766e4
Fedor Ptitsyn <fedya.ptitsyn@mail.ru>
2024-09-29 20:47:32 +0300

    Added base for tooltips

---
+++ Assets/Scripts/UI/InGame/Tooltip.cs
@@ -0,0 +1,35 @@
+﻿using TMPro;
+using UnityEngine;
+using Cursor = UnityEngine.Cursor;
+
+namespace UI.InGame
+{
+    public class Tooltip : MonoBehaviour
+    {
+        public TextMeshProUGUI textComponent;
+
+        private void Start()
+        {
+            Cursor.visible = true;
+            gameObject.SetActive(false);
+        }
+        
+        private void Update()
+        {
+            transform.position = Input.mousePosition;
+        }
+
+        public void SetAndShowTooltip()
+        {
+            Debug.Log("a");
+            // textComponent.text = 
+        }
+        
+        public void HideTooltip()
+        {
+            Debug.Log("b");
+            gameObject.SetActive(false);
+            textComponent.text = string.Empty;
+        }
+    }
+}
