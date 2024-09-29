aae009ce4bdeec6145d3e312d7b05dce3193b56b
Fedor Ptitsyn <fedya.ptitsyn@mail.ru>
2024-09-29 21:13:44 +0300

    gg

--- Assets/Scripts/UI/InGame/Tooltip.cs
+++ Assets/Scripts/UI/InGame/Tooltip.cs
@@ -7,9 +7,11 @@ namespace UI.InGame
     public class Tooltip : MonoBehaviour
     {
         public TextMeshProUGUI textComponent;
+        private GameObject _tooltipObj;
 
         private void Start()
         {
+            _tooltipObj = GameObject.FindGameObjectWithTag("Tooltip");
             Cursor.visible = true;
             gameObject.SetActive(false);
         }
@@ -19,10 +21,26 @@ namespace UI.InGame
             transform.position = Input.mousePosition;
         }
 
-        public void SetAndShowTooltip()
+        public void SetAndShowTooltip(GameObject obj)
         {
+            gameObject.SetActive(true);
             Debug.Log("a");
-            // textComponent.text = 
+            float cost = 0;
+            if (obj.TryGetComponent(out PassiveAbilities passive))
+            {
+                cost = passive.Cost(obj.name);
+            }
+            else if (obj.TryGetComponent(out ActiveAbility active))
+            {
+                cost = active.Cost(obj.name);
+            }
+            else
+            {
+                Debug.Log("couldn't find script");
+            }
+            Debug.Log(cost);
+            Debug.Log(gameObject);
+            textComponent.text = $"Стоимость: {cost}";
         }
         
         public void HideTooltip()
